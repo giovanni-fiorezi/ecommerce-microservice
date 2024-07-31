d523f541406313cd3713000a508ce0b2170fc801
giovanni-fiorezi <giovannifgiovanelli@gmail.com>
2024-07-31 14:31:42 -0300

    Crud de produtos finalizado e upload de planilha dos produtos finalizado.

--- ecommerce/src/main/java/br/com/projeto/ecommerce/resource/ProductResource.java
+++ ecommerce/src/main/java/br/com/projeto/ecommerce/resource/ProductResource.java
@@ -1,13 +1,14 @@
 package br.com.projeto.ecommerce.resource;
 
 import br.com.projeto.ecommerce.dto.ProductDto;
+import br.com.projeto.ecommerce.enums.ProductCategory;
 import br.com.projeto.ecommerce.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,14 +23,38 @@ public class ProductResource {
 
     @GetMapping
     public ResponseEntity<List<ProductDto>> findAll() {
-        productService.findAll();
-        return ResponseEntity.ok().build();
+        List<ProductDto> products = productService.findAll();
+        return ResponseEntity.ok().body(products);
+    }
+
+    @GetMapping("/find-by-category")
+    public ResponseEntity<List<ProductDto>> findProductsByCategory(@RequestParam("category")ProductCategory productCategory) {
+        List<ProductDto> products = productService.findProductsByCategory(productCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDto> findById(@PathVariable Long productId) {
         ProductDto dto = productService.findById(productId);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> saveProduct(@RequestBody ProductDto productDto) {
+        productService.saveProduct(productDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado com sucesso!");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> saveProduct(@PathVariable Long id) {
+        productService.removeProduct(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Produto removido com sucesso!");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file) throws IOException {
+        productService.readAndSaveExcel(file);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Dados do arquivo salvo com sucesso!");
     }
 
 }
--- ecommerce/src/main/java/br/com/projeto/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/br/com/projeto/ecommerce/service/ProductService.java
@@ -2,13 +2,23 @@ package br.com.projeto.ecommerce.service;
 
 import br.com.projeto.ecommerce.dto.ProductDto;
 import br.com.projeto.ecommerce.enums.ProductCategory;
+import br.com.projeto.ecommerce.mapper.ModelMapper;
 import br.com.projeto.ecommerce.models.Product;
 import br.com.projeto.ecommerce.repository.ProductRepository;
-import org.modelmapper.ModelMapper;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,9 +28,6 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    @Autowired
-    private ModelMapper modelMapper;
-
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
@@ -28,8 +35,7 @@ public class ProductService {
     public List<ProductDto> findAll() {
         try {
             List<Product> products = productRepository.findAll();
-            return products.stream().map(
-                    p -> modelMapper.map(p, ProductDto.class)).collect(Collectors.toList());
+            return ModelMapper.parseListObjects(products, ProductDto.class);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Erro ao buscar todos os produtos!");
@@ -41,7 +47,7 @@ public class ProductService {
             List<Product> products = productRepository.findAll();
             return products.stream()
                     .filter(product -> product.getProductCategory().equals(productCategory))
-                    .map(p -> modelMapper.map(p, ProductDto.class))
+                    .map(p -> ModelMapper.parseObject(p, ProductDto.class))
                     .collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,7 +58,7 @@ public class ProductService {
     public ProductDto findById(Long id) {
         if(id != null) {
             Optional<Product> product = productRepository.findById(id);
-            return modelMapper.map(product, ProductDto.class);
+            return ModelMapper.parseObject(product, ProductDto.class);
         } else {
             throw new RuntimeException("Insira um id para fazer a busca!");
         }
@@ -64,7 +70,7 @@ public class ProductService {
             if(dto == null) {
                 throw new RuntimeException("Insira os dados do produto!");
             }
-            Product product = modelMapper.map(dto, Product.class);
+            Product product = ModelMapper.parseObject(dto, Product.class);
             productRepository.save(product);
         } catch (Exception e) {
             e.printStackTrace();
@@ -85,6 +91,29 @@ public class ProductService {
         );
     }
 
+    public void readAndSaveExcel(MultipartFile file) throws IOException {
+        try {
+            InputStream inputStream = file.getInputStream();
+            Workbook workbook = new XSSFWorkbook(inputStream);
 
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) continue;
+
+                ProductDto dto = new ProductDto();
+                dto.setName(row.getCell(0).toString());
+                dto.setDescription(row.getCell(1).toString());
+                dto.setProductCategory(ProductCategory.valueOf(row.getCell(2).getStringCellValue()));
+                dto.setAmount((int) row.getCell(3).getNumericCellValue());
+                dto.setPrice(BigDecimal.valueOf(row.getCell(4).getNumericCellValue()));
+
+                Product product = ModelMapper.parseObject(dto, Product.class);
+                productRepository.save(product);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
